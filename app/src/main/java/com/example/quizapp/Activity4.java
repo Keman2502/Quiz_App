c4ef24f6044814966a80d2ad2865124158a4a1c0
Eman Khan <keman2502@gmail.com>
2021-11-09 00:47:35 +0500

    Implement Design of Practice Layout

--- app/src/main/java/com/example/quizapp/Activity4.java
+++ app/src/main/java/com/example/quizapp/Activity4.java
@@ -2,7 +2,18 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity4 extends AppCompatActivity {
 
