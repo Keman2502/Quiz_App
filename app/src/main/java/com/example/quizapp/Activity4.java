39d8d2b7289fdb9fc82a6666f8cd39034fc7996f
Eman Khan <keman2502@gmail.com>
2021-11-09 00:52:03 +0500

    Java Class Implementation for Activity 4

--- app/src/main/java/com/example/quizapp/Activity4.java
+++ app/src/main/java/com/example/quizapp/Activity4.java
@@ -17,9 +17,119 @@ import java.util.List;
 
 public class Activity4 extends AppCompatActivity {
 
+    private TextView tvQuestion,tvQuestionNo;
+    private RadioGroup radioGroup;
+    private RadioButton rb1,rb2,rb3,rb4;
+    private Button btnNext;
+
+    int totalQuestions;
+    int qCounter=0;
+
+
+    ColorStateList dfRbColor;
+    boolean answered;
+
+    private QuestionModel currentQuestion;
+    private List<QuestionModel> questionsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+
+        questionsList = new ArrayList<>();
+        tvQuestion= findViewById(R.id.textQuestion);
+        tvQuestionNo= findViewById(R.id.textQuestionNo);
+
+        radioGroup= findViewById(R.id.radioGroup);
+        rb1= findViewById(R.id.rb1);
+        rb2= findViewById(R.id.rb2);
+        rb3= findViewById(R.id.rb3);
+        rb4= findViewById(R.id.rb4);
+        btnNext= findViewById(R.id.btnNext);
+
+        dfRbColor = rb1.getTextColors();
+
+        addQuestions();
+        totalQuestions = questionsList.size();
+        showNextQuestion();
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (answered == false){
+                    if(rb1.isChecked() || rb2.isChecked() || rb3.isChecked()  || rb4.isChecked()){
+                        checkAnswer();
+                    }else{
+                        Toast.makeText(Activity4.this, "Please Select an option", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    showNextQuestion();
+                }
+            }
+        });
+    }
+    private void checkAnswer() {
+        answered = true;
+        RadioButton rbSelected = findViewById(radioGroup.getCheckedRadioButtonId());
+        int answerNo = radioGroup.indexOfChild(rbSelected) +1;
+
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+        rb4.setTextColor(Color.RED);
+
+        switch (currentQuestion.getCorrectAnsNo()){
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                break;
+            case 4:
+                rb4.setTextColor(Color.GREEN);
+                break;
+        }
+        if (qCounter < totalQuestions){
+            btnNext.setText("Next");
+        }else{
+            btnNext.setText("Finish");
+        }
+    }
+
+    private void showNextQuestion() {
+
+        radioGroup.clearCheck();
+        rb1.setTextColor(dfRbColor);
+        rb2.setTextColor(dfRbColor);
+        rb3.setTextColor(dfRbColor);
+        rb4.setTextColor(dfRbColor);
+
+        if(qCounter < totalQuestions){
+            currentQuestion = questionsList.get(qCounter);
+            tvQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+            rb4.setText(currentQuestion.getOption4());
+
+            qCounter++;
+
+            btnNext.setText("Submit");
+            tvQuestionNo.setText("Question: " + qCounter + "/" + totalQuestions);
+            answered=false;
+        }else{
+            finish();
+        }
+    }
+
+    private void addQuestions() {
+        questionsList.add(new QuestionModel("A is correct?","A","B","C","D",1));
+        questionsList.add(new QuestionModel("B is correct?","A","B","C","D",2));
+        questionsList.add(new QuestionModel("C is correct?","A","B","C","D",3));
+        questionsList.add(new QuestionModel("D is correct?","A","B","C","D",4));
     }
 }
